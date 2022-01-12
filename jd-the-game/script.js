6c55df72fa1e11cfd89c81376aa3d28e7e510136
MaciejkaG <maciejg2008@gmail.com>
2022-01-12 20:33:33 +0100

    bug fixes

---
+++ jd-the-game/script.js
@@ -0,0 +1,8 @@
+function checkandredirect() {
+    let passwd = document.getElementsByClassName("passwdBox")[0].value
+    if (passwd=="jebacdisa1337") {
+        location.replace("32184623894739812471831846321847138463184631841364816384613846184638137286829748926879658268463286418376816478197321746319761382.html");
+    } else {
+        alert("Hasło nieprawidłowe :(");
+    }
+}
